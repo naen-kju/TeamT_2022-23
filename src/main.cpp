6c276f6a15594e8e919bd4cba98ec9ae2e74960a
aWildMilo <awildmilo@proton.me>
2022-06-28 09:34:11 -0700

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+void pre_auton(void) 
+{
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // Insert code here
+}
+
+
+void autonomous(void) 
+{
+  // Insert code here
+}
+
+
+void usercontrol(void) 
+{
+  while (true) 
+  {
+    // Insert code here
+
+    wait(20, msec);
+  }
+}
+
+
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
