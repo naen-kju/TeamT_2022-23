ef231217242d2b9d2310c07f6deb14b9f9fe6992
aWildMilo <awildmilo@pm.me>
2022-07-04 19:02:37 -0700

    Added new sensors + header guards. Deleted notes.
    Sensors added:
    - inertial sensor
    - gps sensor
    - optical sensor
    - distance sensor
    
    Removed comments temporarily.
    
    Added header guards.

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,19 +1,26 @@
+#ifndef robot_config_h
+#define robot_config_h
+
 using namespace vex;
+
+using signature = vision::signature;
+using code = vision::code;
 
 extern brain Cortex;
 
-// VEXcode devices
+extern controller Remix;
+extern controller Wild;
 extern motor LF;
 extern motor LB;
 extern motor RF;
 extern motor RB;
 extern motor Fly;
-extern controller Remix;
-extern controller Wild;
+extern inertial Vigour;
+extern gps Pilot;
+extern optical Chroma;
+extern distance Ambit;
 
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
- * This should be called at the start of your int main function.
- */
+// Used to initialize code/tasks/devices added using tools in VEXcode Pro.
 void  vexcodeInit ( void );
+
+#endif
