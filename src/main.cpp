a39d866f3d6546c433ab3b2313cecbf48c350c4c
aWildMilo <awildmilo@pm.me>
2022-07-12 09:37:48 -0700

    Fixed mecanum code. Changed wait time, etc.
    Issues with switching between controllers.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@ competition Match;
 
 void pre_auton( void )
 {
-
 }
 
 void auton ( void )
@@ -15,8 +14,8 @@ void auton ( void )
 void user ( void )
 {
   const int
-  MaxThres = 75,
-  MinThres = 0;
+  MaxThres { 75 },
+  MinThres { 0 };
 
   int 
   Y1Thres, X1Thres, R1Thres,
@@ -24,39 +23,33 @@ void user ( void )
 
   while (true) 
   {
-    Remix.ButtonA.pressed(RemixEnable);
-    Wild.ButtonA.pressed(WildEnable);
+    Remix.ButtonA.pressed ( RemixEnable );
+    Wild.ButtonA.pressed ( WildEnable );
 
     Y1Thres = Remix.Axis3.position ( pct ),
     X1Thres = Remix.Axis4.position ( pct ),
-    R1Thres = Remix.Axis2.position ( pct );
+    R1Thres = Remix.Axis1.position ( pct );
  
     Y2Thres = Wild.Axis3.position ( pct ),
     X2Thres = Wild.Axis4.position ( pct ),
-    R2Thres = Wild.Axis2.position ( pct );
+    R2Thres = Wild.Axis1.position ( pct );
 
-    if (RemixControlEnabled() == true)
+    if ( RemixControlEnabled () == true )
     {
       Thresholds ( Y1Thres, MaxThres, MinThres );
       Thresholds ( X1Thres, MaxThres, MinThres );
       Thresholds ( R1Thres, MaxThres, MinThres );
-      LF.spin ( fwd, X1Thres + Y1Thres + R1Thres, pct );
-      LB.spin ( fwd, -X1Thres - Y1Thres - R1Thres, pct );
-      RF.spin ( fwd, X1Thres - Y1Thres - R1Thres, pct );
-      RB.spin ( fwd, -X1Thres - Y1Thres - R1Thres, pct );
+      MecanumSpin ( Y1Thres, X1Thres, R1Thres );
     }
-    if (WildControlEnabled() == true)
+    if ( WildControlEnabled () == true )
     {
       Thresholds ( Y2Thres, MaxThres, MinThres );
       Thresholds ( X2Thres, MaxThres, MinThres );
       Thresholds ( R2Thres, MaxThres, MinThres );
-      LF.spin ( fwd, X2Thres + Y2Thres + R2Thres, pct );
-      LB.spin ( fwd, -X2Thres - Y2Thres - R2Thres, pct );
-      RF.spin ( fwd, X2Thres - Y2Thres - R2Thres, pct );
-      RB.spin ( fwd, -X2Thres - Y2Thres - R2Thres, pct );
+      MecanumSpin ( Y2Thres, X2Thres, R2Thres );
     }
    
-    wait ( 16 + ( 2 / 3 ), msec );
+    wait ( 20, msec );
   }
 }
 
