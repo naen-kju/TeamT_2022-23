a10f0bbc0c0afa18912a041a64780e2a3cb8bc70
aWildMilo <awildmilo@pm.me>
2022-07-12 09:32:25 -0700

    Fixed drive motor config, added new motors

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -4,11 +4,13 @@ brain  Cortex;
 
 controller Remix = controller ( primary );
 controller Wild = controller ( partner );
-motor LF = motor ( PORT1, ratio18_1, false );
-motor LB = motor ( PORT2, ratio18_1, false );
-motor RF = motor ( PORT3, ratio18_1, true );
-motor RB = motor ( PORT4, ratio18_1, true );
+motor LF = motor ( PORT2, ratio6_1, false );
+motor LB = motor ( PORT11, ratio6_1, false );
+motor RF = motor ( PORT10, ratio6_1, true );
+motor RB = motor ( PORT19, ratio6_1, true );
 motor Fly = motor ( PORT5, ratio6_1, false);
+motor Intake = motor ( PORT6, ratio6_1, false);
+motor Angle = motor ( PORT7, ratio18_1, false);
 inertial Vigour = inertial(PORT6);
 gps Pilot = gps ( PORT7, 0, 0, mm, 0, right );
 optical Chroma = optical ( PORT8, true );
