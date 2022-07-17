836e56d7ab11806ce8489cd3aac3e249c0ada60c
aWildMilo <awildmilo@pm.me>
2022-07-16 18:08:13 -0700

    Slightly changed formatting

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -4,13 +4,16 @@ brain  Cortex;
 
 controller Remix = controller ( primary );
 controller Wild = controller ( partner );
+
 motor LF = motor ( PORT2, ratio6_1, false );
-motor LB = motor ( PORT11, ratio6_1, false );
 motor RF = motor ( PORT10, ratio6_1, true );
+motor LB = motor ( PORT11, ratio6_1, false );
 motor RB = motor ( PORT19, ratio6_1, true );
+
 motor Fly = motor ( PORT5, ratio6_1, false);
 motor Intake = motor ( PORT6, ratio6_1, false);
 motor Angle = motor ( PORT7, ratio18_1, false);
+
 inertial Vigour = inertial(PORT6);
 gps Pilot = gps ( PORT7, 0, 0, mm, 0, right );
 optical Chroma = optical ( PORT8, true );
