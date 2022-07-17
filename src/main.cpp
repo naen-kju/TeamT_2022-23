ac3cc7c6b02afd9e2b003cc11367523c536ffe3c
aWildMilo <awildmilo@pm.me>
2022-07-16 18:10:36 -0700

    No longer needs button enable, update auton.

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,16 @@ void pre_auton( void )
 
 void auton ( void )
 {
-
+  MecanumMoveTheta (  90, 1, 1000 );
+  MecanumMoveTheta (  180, 1, 1000 );
+  MecanumMoveTheta (  270, 1, 1000 );
+  MecanumMoveTheta (  0, 1, 1000 );
 }
 
 void user ( void )
 {
   const int
-  MaxThres { 75 },
-  MinThres { 0 };
+  MaxThres { 75 };
 
   int 
   Y1Thres, X1Thres, R1Thres,
@@ -23,9 +25,6 @@ void user ( void )
 
   while (true) 
   {
-    Remix.ButtonA.pressed ( RemixEnable );
-    Wild.ButtonA.pressed ( WildEnable );
-
     Y1Thres = Remix.Axis3.position ( pct ),
     X1Thres = Remix.Axis4.position ( pct ),
     R1Thres = Remix.Axis1.position ( pct );
@@ -33,21 +32,9 @@ void user ( void )
     Y2Thres = Wild.Axis3.position ( pct ),
     X2Thres = Wild.Axis4.position ( pct ),
     R2Thres = Wild.Axis1.position ( pct );
-
-    if ( RemixControlEnabled () == true )
-    {
-      Thresholds ( Y1Thres, MaxThres, MinThres );
-      Thresholds ( X1Thres, MaxThres, MinThres );
-      Thresholds ( R1Thres, MaxThres, MinThres );
-      MecanumSpin ( Y1Thres, X1Thres, R1Thres );
-    }
-    if ( WildControlEnabled () == true )
-    {
-      Thresholds ( Y2Thres, MaxThres, MinThres );
-      Thresholds ( X2Thres, MaxThres, MinThres );
-      Thresholds ( R2Thres, MaxThres, MinThres );
-      MecanumSpin ( Y2Thres, X2Thres, R2Thres );
-    }
+  
+    DriverControl ( Y1Thres, X1Thres, R1Thres, MaxThres );
+    DriverControl ( Y2Thres, X2Thres, R2Thres, MaxThres );
    
     wait ( 20, msec );
   }
